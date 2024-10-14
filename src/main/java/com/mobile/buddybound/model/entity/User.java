c1a4015c04ea4493e889c7f2a1aa76c2cb3da574
Phu Le <21522466@gm.uit.edu.vn>
2024-10-14 23:53:53 +0700

    add necessary libs + updaate application.yml

--- src/main/java/com/mobile/buddybound/model/entity/User.java
+++ src/main/java/com/mobile/buddybound/model/entity/User.java
@@ -1,4 +1,24 @@
 package com.mobile.buddybound.model.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "hash_password")
+    private String hashPassword;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Column(name = "created_at")
+    private LocalDate createdAt;
+    @Column(name = "updated_at")
+    private LocalDate updatedAt;
 }
