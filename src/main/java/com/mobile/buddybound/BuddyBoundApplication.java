e79c1a311fc4a3800450c299a8244beb5af0a1a9
Phu Le <21522466@gm.uit.edu.vn>
2024-12-02 00:03:14 +0700

    [Async] optimize file uploading by using asynchronous image upload

--- src/main/java/com/mobile/buddybound/BuddyBoundApplication.java
+++ src/main/java/com/mobile/buddybound/BuddyBoundApplication.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @SecurityScheme(name = "bearerAuth", scheme = "bearer", bearerFormat = "JWT", type= SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 @EnableScheduling
+@EnableAsync
 public class BuddyBoundApplication {
 
     public static void main(String[] args) {
